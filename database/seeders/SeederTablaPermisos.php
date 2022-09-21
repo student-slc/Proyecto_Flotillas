0662a1f3dae0ec7e098adb37c4e230b12031ca79
Cesar Segura Landa <cesar_201723083@test.edu.mx>
2022-09-21 11:28:09 -0500

    Proyecto

---
+++ database/seeders/SeederTablaPermisos.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Spatie\Permission\Models\Permission;
+class SeederTablaPermisos extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $permisos=[
+            'ver-rol',
+            'crear-rol',
+            'editar-rol',
+            'borrar-rol',
+
+            'ver-blog',
+            'crear-blog',
+            'editar-blog',
+            'borrar-blog',
+
+        ];
+        foreach($permisos as $permiso){
+            Permission::create(['name'=>$permiso]);
+        }
+    }
+}
---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,14 @@
+<li class="side-menus {{ Request::is('home') ? 'active' : '' }}">
+    <a class="nav-link" href="/">
+        <i class=" fas fa-building"></i><span>Dashboard</span>
+    </a>
+    <a class="nav-link" href="/usuarios">
+        <i class=" fas fa-users"></i><span>Usuario</span>
+    </a>
+    <a class="nav-link" href="/roles">
+        <i class=" fas fa-user-lock"></i><span>Roles</span>
+    </a>
+    <a class="nav-link" href="/blogs">
+        <i class=" fas fa-blog"></i><span>Blogs</span>
+    </a>
+</li>
