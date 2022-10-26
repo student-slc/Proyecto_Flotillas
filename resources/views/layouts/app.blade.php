ed0c5036eed8d4e2e12a4a48244500e40b650ce0
Cesar Segura Landa <cesar_201723083@test.edu.mx>
2022-10-26 17:00:31 -0500

    Correccion_select2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
-    <meta name="csrf-token" content="{{ csrf_token() }}"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>@yield('title')</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-
-
     <!-- Bootstrap 4.1.1 -->
-    {{-- <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" /> --}}
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- Ionicons -->
@@ -23,9 +22,10 @@
     <!-- resources/views/welcome.blade.php -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-2.2.4.min.js" ></script>
-     <link href="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.1/css/selectize.bootstrap3.min.css" rel="stylesheet" />
-    <meta name="theme-color" content="#6777ef"/>
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.1/css/selectize.bootstrap3.min.css"
+        rel="stylesheet" />
+    <meta name="theme-color" content="#6777ef" />
     <link rel="apple-touch-icon" href="{{ asset('logo.PNG') }}">
     <link rel="manifest" href="{{ asset('/manifest.json') }}">
 
@@ -46,30 +46,31 @@
         });
     }
 </script> --}}
+
 <body>
 
-<div id="app">
-    <div class="main-wrapper main-wrapper-1">
-        <div class="navbar-bg"></div>
-        <nav class="navbar navbar-expand-lg main-navbar">
-            @include('layouts.header')
+    <div id="app">
+        <div class="main-wrapper main-wrapper-1">
+            <div class="navbar-bg"></div>
+            <nav class="navbar navbar-expand-lg main-navbar">
+                @include('layouts.header')
 
-        </nav>
-        <div class="main-sidebar main-sidebar-postion">
-            @include('layouts.sidebar')
+            </nav>
+            <div class="main-sidebar main-sidebar-postion">
+                @include('layouts.sidebar')
+            </div>
+            <!-- Main Content -->
+            <div class="main-content">
+                @yield('content')
+            </div>
+            <footer class="main-footer">
+                @include('layouts.footer')
+            </footer>
         </div>
-        <!-- Main Content -->
-        <div class="main-content">
-            @yield('content')
-        </div>
-        <footer class="main-footer">
-            @include('layouts.footer')
-        </footer>
     </div>
-</div>
 
-@include('profile.change_password')
-@include('profile.edit_profile')
+    @include('profile.change_password')
+    @include('profile.edit_profile')
 
 </body>
 <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
@@ -91,12 +92,12 @@
 @yield('page_js')
 @yield('scripts')
 <script>
-    let loggedInUser =@json(\Illuminate\Support\Facades\Auth::user());
+    let loggedInUser = @json(\Illuminate\Support\Facades\Auth::user());
     let loginUrl = '{{ route('login') }}';
-    const userUrl = '{{url('users')}}';
+    const userUrl = '{{ url('users') }}';
     // Loading button plugin (removed from BS4)
-    (function ($) {
-        $.fn.button = function (action) {
+    (function($) {
+        $.fn.button = function(action) {
             if (action === 'loading' && this.data('loading-text')) {
                 this.data('original-text', this.html()).html(this.data('loading-text')).prop('disabled', true);
             }
@@ -107,4 +108,5 @@
     }(jQuery));
 </script>
 @yield('modal')
+
 </html>
