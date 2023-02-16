bcdc228dc6b6af73069eabbff4f129e70f2e1959
Abraham-Lara <90463756+Abraham-Lara@users.noreply.github.com>
2023-02-16 14:58:04 -0600

    Update index.blade.php

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <section class="section">
         <div class="section-header">
-            <h3 class="page__heading">Clientes</h3>
+            <h3 class="page__heading">Clientes 2</h3>
         </div>
         <div class="section-body">
             <div class="row">
